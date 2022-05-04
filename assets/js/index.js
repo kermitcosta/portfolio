7c36b7a36c486518608de54cf88ceb98ca228714
kermitcosta <kermitcst@gmail.com>
2022-05-04 15:49:01 -0300

    menu added

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// Menu mobile //
+
+//Função para mostrar o menu
+function showMenu() {
+  document.querySelector('.menu').style.display = 'block'
+}
+
+//Função para fechar o menu
+function closeMenu() {
+  document.querySelector('.menu').style.display = 'none'
+}
