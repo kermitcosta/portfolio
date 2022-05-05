9bb9f3ecd5265ab8c25df420fde47de215862369
kermitcosta <kermitcst@gmail.com>
2022-05-05 12:20:57 -0300

    updated menu with new functionalities

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,32 @@
-// Menu mobile //
+// Menu //
 
-//Função para mostrar o menu
+//Função para mostrar o menu na versão mobile
 function showMenu() {
   document.querySelector('.menu').style.display = 'block'
 }
 
-//Função para fechar o menu
+//Função para fechar o menu na versão mobile
 function closeMenu() {
   document.querySelector('.menu').style.display = 'none'
 }
+
+//Ativar menu quando muda a seção
+const sections = document.querySelectorAll('section')
+const navLi = document.querySelectorAll('nav ul li a')
+
+window.addEventListener('scroll', () => {
+  let current = ''
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.clientHeight
+    if (scrollY >= sectionTop - sectionHeight) {
+      current = section.getAttribute('id')
+    }
+  })
+  navLi.forEach(a => {
+    a.classList.remove('active')
+    if (a.classList.contains(current)) {
+      a.classList.add('active')
+    }
+  })
+})
