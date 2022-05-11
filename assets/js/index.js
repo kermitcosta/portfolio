45807fe78658e5ad4fd5bb0582d933b259231fcb
kermitcosta <kermitcst@gmail.com>
2022-05-11 15:42:24 -0300

    added reveal effect in sections

--- assets/js/index.js
+++ assets/js/index.js
@@ -43,3 +43,24 @@ function getGitHubProfilePicture() {
 }
 
 getGitHubProfilePicture()
+
+//Reveal effect
+window.addEventListener('scroll', reveal)
+
+function reveal() {
+  let reveals = document.querySelectorAll('.reveal')
+
+  for (let i = 0; i < reveals.length; i++) {
+    {
+      let windowHeight = window.innerHeight
+      let revealTop = reveals[i].getBoundingClientRect().top
+      let revealPoint = 150
+
+      if (revealTop < windowHeight - revealPoint) {
+        reveals[i].classList.add('active-reveal')
+      } else {
+        reveals[i].classList.remove('active-reveal')
+      }
+    }
+  }
+}
