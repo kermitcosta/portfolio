c3c69d163b5edd870a424b650e832d07cc16cae8
kermitcosta <kermitcst@gmail.com>
2022-05-16 17:01:50 -0300

    navbar rearranged with bootstrap

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,3 @@
-// Menu //
-
-//Show mobile version menu
-function showMenu() {
-  document.querySelector('.menu').style.display = 'block'
-}
-
-//Hide mobile version menu
-function closeMenu() {
-  document.querySelector('.menu').style.display = 'none'
-}
-
-//Activate menu based on section
-const sections = document.querySelectorAll('section')
-const navLi = document.querySelectorAll('nav ul li a')
-
-window.addEventListener('scroll', () => {
-  let current = ''
-  sections.forEach(section => {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.clientHeight
-    if (scrollY >= sectionTop - (sectionHeight - 100) / 3) {
-      current = section.getAttribute('id')
-    }
-  })
-  navLi.forEach(a => {
-    a.classList.remove('active')
-    if (a.classList.contains(current)) {
-      a.classList.add('active')
-    }
-  })
-})
-
 //Get GitHub Profile Picture
 function getGitHubProfilePicture() {
   const url = 'https://api.github.com/users/kermitcosta'
