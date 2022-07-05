82c79fda9ae5a8d3237fc087da68dfdb0f272c4a
kermitcosta <kermitcst@gmail.com>
2022-07-05 12:35:07 -0300

    formjs modification

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,36 +5,36 @@ const pageComments = []
 
 // hear the click on the submit button to post the comment
 formButton.addEventListener('click', function () {
-  // selection of all form inputs
-  let form = document.querySelector('#comment-form')
-  let formName = document.querySelector('.form-name').value
-  let formEmail = document.querySelector('.form-email').value
-  let formMessage = document.querySelector('.form-message').value
+    // selection of all form inputs
+    let form = document.querySelector('#comment-form')
+    let formName = document.querySelector('.form-name').value
+    let formEmail = document.querySelector('.form-email').value
+    let formMessage = document.querySelector('.form-message').value
 
-  //creates an object with the input data
-  let newComment = {
-    name: formName,
-    email: formEmail,
-    message: formMessage
-  }
+    //creates an object with the input data
+    let newComment = {
+        name: formName,
+        email: formEmail,
+        message: formMessage
+    }
 
-  // check input validation (form-validation.js)
-  if (formValidation(newComment)) {
-    //add object to array
-    pageComments.push(newComment)
+    // check input validation (form-validation.js)
+    if (formValidation(newComment)) {
+        //add object to array
+        pageComments.push(newComment)
 
-    //add object to localStorage
-    localStorage.setItem('comments', JSON.stringify(pageComments))
+        //add object to localStorage
+        localStorage.setItem('comments', JSON.stringify(pageComments))
 
-    //clear error messages
-    errorMessage.textContent = ''
+        //clear error messages
+        errorMessage.textContent = ''
 
-    //render comments (comment-render.js)
-    renderComment(pageComments)
+        //render comments (comment-render.js)
+        renderComment(pageComments)
 
-    //clear all form fields
-    form.reset()
-  }
+        //clear all form fields
+        form.reset()
+    }
 })
 
 //character counter
@@ -42,7 +42,7 @@ const text_max = 200
 $('#count_message').html('0 / ' + text_max)
 
 $('#floatingTextarea').keyup(function () {
-  var text_length = $('#floatingTextarea').val().length
+    var text_length = $('#floatingTextarea').val().length
 
-  $('#count_message').html(text_length + ' / ' + text_max)
+    $('#count_message').html(text_length + ' / ' + text_max)
 })
